d7a6954a29e616b72c56309701d3475a6c2bf8e4
IoT-Arduino <bunkajin@icloud.com>
2022-01-15 12:43:36 +0900

    翻訳ファイル変更

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,29 @@
+import en from "../locales/en";
+import jp from "../locales/jp";
+import cn from "../locales/cn";
+
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "@/styles/Footer.module.css";
 
-export default function Footer() {
+export default function Footer({ content }) {
+  const router = useRouter();
+  const { locale } = router;
+
+  let t;
+
+  if (locale === "en-US") {
+    t = en;
+  } else if (locale === "zh-CN") {
+    t = cn;
+  } else {
+    t = jp;
+  }
   return (
     <footer className={styles.footer}>
-      <p>Copytight &copy: DJ Events 2021</p>
+      <p>Copytight &copy; Next&Strapi i18n Web Site</p>
       <p>
-        <Link href="/about">About This Project</Link>
+        <Link href="/about">{t.about}</Link>
       </p>
     </footer>
   );
